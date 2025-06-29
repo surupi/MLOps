06ae7df7c76ceb2c430021c9757d3258dee981d8
surupi <surupi.nandi@gmail.com>
2025-06-29 15:01:57 +0000

    Iris pipeline

--- train.py
+++ train.py
@@ -8,21 +8,26 @@ from sklearn.tree import DecisionTreeClassifier
 
 data = pd.read_csv('iris.csv')
 
-train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
-X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+train, test = train_test_split(data, test_size=0.4, stratify=data['species'], random_state=42)
+X_train = train[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
 y_train = train.species
-X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+X_test = test[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
 y_test = test.species
 
 print('Training start')
-mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
-mod_dt.fit(X_train,y_train)
-prediction=mod_dt.predict(X_test)
+mod_dt = DecisionTreeClassifier(max_depth=3, random_state=1)
+mod_dt.fit(X_train, y_train)
+prediction = mod_dt.predict(X_test)
 print('Training Complete')
-print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
 
+accuracy = metrics.accuracy_score(prediction, y_test)
+print('The accuracy of the Decision Tree is', "{:.3f}".format(accuracy))
 
 os.makedirs('models', exist_ok=True)
 with open('models/week2_decisionTree_model.pkl', 'wb') as f:
     pickle.dump(mod_dt, f)
 print('Model saved')
+
+with open('metrics.txt', 'w') as f:
+    f.write(f"Test Accuracy: {accuracy:.3f}\n")
+print('Metrics saved')
